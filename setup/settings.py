3960f906a7543bffb00b08cf8de5f5bd8d2f0446
marcocapozzoli <marcocapozzoli90@gmail.com>
2021-11-20 20:46:56 -0300

    add model

--- setup/settings.py
+++ setup/settings.py
@@ -31,12 +31,22 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Django Apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # 3rd Apps
+    'rest_framework',
+    'rest_framework.authtoken',
+    'drf_yasg',
+    'django_filters',
+    
+    # Local Apps
+    'employees',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +137,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Django REST Framework
+REST_FRAMEWORK = {
+    'SEARCH_PARAM': 'username'
+}
