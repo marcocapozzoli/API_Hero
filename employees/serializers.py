a87e7836b6dab4fce5262ff95dbf5e8c58aa1232
marcocapozzoli <marcocapozzoli90@gmail.com>
2021-11-20 20:59:35 -0300

    add create serializer

--- employees/serializers.py
+++ employees/serializers.py
@@ -33,3 +33,54 @@ class ListEmployeeSerializer(serializers.ModelSerializer):
             'cpf',
             'companies'
         ]
+        
+class CreateEmployeeSerializer(serializers.ModelSerializer):
+    first_name = serializers.CharField(write_only=True)
+    last_name = serializers.CharField(write_only=True)
+    username = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True)
+    full_name = serializers.CharField(source='user.get_full_name', read_only=True)
+    
+    class Meta:
+        model = Employee
+        fields = [
+            'id',
+            'first_name',
+            'last_name',
+            'username',
+            'password',
+            'full_name',
+            'cpf'
+        ]
+
+    def validate(self, data):
+        if User.objects.filter(username=data['username']):
+            raise serializers.ValidationError({
+                "username": f"{data['username']} " + "já cadastrado no banco de dados."
+            })
+        if not validate_cpf(data.get('cpf', '')):
+            raise serializers.ValidationError(
+                {'cpf': 'Um cpf válido é obrigatório.'}
+            )
+        return data
+        
+    def create(self, validated_data):
+        cpf = validated_data.pop('cpf')
+        user = User.objects.create(**validated_data)
+        instance = Employee.objects.create(user=user, cpf=cpf)
+        instance.save()
+        return instance
+    
+    def update(self, instance, validated_data):
+    
+        instance.user.first_name = validated_data["first_name"]
+        instance.user.last_name = validated_data["last_name"]
+        instance.user.username = validated_data["username"]
+        instance.user.set_password(validated_data["password"])
+        instance.user.save()
+        instance.cpf = validated_data["cpf"]
+        instance.save()
+        
+        return instance
+  
+  
