4e3f0e3f238f4767c412ce6b347a444bfb435dde
marcocapozzoli <marcocapozzoli90@gmail.com>
2021-11-20 21:20:45 -0300

    list company

--- companies/views.py
+++ companies/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Company
+from .serializers import CreateCompanySerializer, ListCompanySerializer
+
+
+class ListCompanyAPIView(generics.ListCreateAPIView):
+    queryset = Company.objects.all()
+    def get_serializer_class(self):
+        if self.request.method == 'GET':
+            return ListCompanySerializer
+        elif self.request.method == 'POST':
+            return CreateCompanySerializer
